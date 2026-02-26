6769e83865aa3b55e6fd1882670adb4ef2421cbc
Fabrício Bracht <fabracht@gmail.com>
2026-02-25 17:34:49 -0800

    add per-subscription and per-publish strategies to HOL blocking experiment

--- experiments/run/02_hol_blocking.sh
+++ experiments/run/02_hol_blocking.sh
@@ -10,15 +10,19 @@ declare -A TRANSPORT_URLS
 TRANSPORT_URLS[tcp]="mqtt://${BROKER_IP}:1883"
 TRANSPORT_URLS[quic-control]="quic://${BROKER_IP}:14567"
 TRANSPORT_URLS[quic-pertopic]="quic://${BROKER_IP}:14567"
+TRANSPORT_URLS[quic-persub]="quic://${BROKER_IP}:14567"
+TRANSPORT_URLS[quic-perpub]="quic://${BROKER_IP}:14567"
 
 declare -A TRANSPORT_FLAGS
 TRANSPORT_FLAGS[tcp]=""
 TRANSPORT_FLAGS[quic-control]="--quic-stream-strategy control-only --ca-cert /opt/mqtt-certs/ca.pem"
 TRANSPORT_FLAGS[quic-pertopic]="--quic-stream-strategy per-topic --ca-cert /opt/mqtt-certs/ca.pem"
+TRANSPORT_FLAGS[quic-persub]="--quic-stream-strategy per-subscription --ca-cert /opt/mqtt-certs/ca.pem"
+TRANSPORT_FLAGS[quic-perpub]="--quic-stream-strategy per-publish --ca-cert /opt/mqtt-certs/ca.pem"
 
 start_broker "--tls-cert /opt/mqtt-certs/server.pem --tls-key /opt/mqtt-certs/server.key --quic-host 0.0.0.0:14567"
 
-for tname in tcp quic-control quic-pertopic; do
+for tname in tcp quic-control quic-pertopic quic-persub quic-perpub; do
     url="${TRANSPORT_URLS[$tname]}"
     flags="${TRANSPORT_FLAGS[$tname]}"
 
